bb4ee791402cb63a63591ead6c4a051969693b4b
snelson59 <snelson59@huskers.unl.edu>
2025-03-28 20:16:56 -0500

    fixed tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -4,7 +4,7 @@ namespace GithubActionsLab;
 public class Addition
 {
 	[TestMethod]
-	public void Add_Valid_Patino()
+	public void Add_Valid_Nelson()
 	{
 		Assert.AreEqual(3, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
@@ -12,7 +12,7 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Invalid_Patino()
+	public void Add_Invalid_Nelson()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
@@ -20,7 +20,7 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Null_Patino()
+	public void Add_Null_Nelson()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
@@ -31,15 +31,15 @@ public class Addition
 public class Subtract
 {
 	[TestMethod]
-	public void Subtract_Valid_Patino()
+	public void Subtract_Valid_Nelson()
 	{
-		Assert.AreEqual(3, Program.Subtract("1", "2"));
-		Assert.AreEqual(5, Program.Subtract("3", "2"));
-		Assert.AreEqual(12, Program.Subtract("5", "7"));
+		Assert.AreEqual(-1, Program.Subtract("1", "2"));
+		Assert.AreEqual(1, Program.Subtract("3", "2"));
+		Assert.AreEqual(0, Program.Subtract("7", "7"));
 	}
 
 	[TestMethod]
-	public void Subtract_Invalid_Patino()
+	public void Subtract_Invalid_Nelson()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Subtract("a", "1"));
@@ -47,7 +47,7 @@ public class Subtract
 	}
 
 	[TestMethod]
-	public void Subtract_Null_Patino()
+	public void Subtract_Null_Nelson()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract(null, "1"));
@@ -59,15 +59,15 @@ public class Subtract
 public class Multiply
 {
 	[TestMethod]
-	public void Multiply_Valid_Patino()
+	public void Multiply_Valid_Nelson()
 	{
-		Assert.AreEqual(3, Program.Multiply("1", "2"));
-		Assert.AreEqual(5, Program.Multiply("3", "2"));
-		Assert.AreEqual(12, Program.Multiply("5", "7"));
+		Assert.AreEqual(2, Program.Multiply("1", "2"));
+		Assert.AreEqual(6, Program.Multiply("3", "2"));
+		Assert.AreEqual(35, Program.Multiply("5", "7"));
 	}
 
 	[TestMethod]
-	public void Multiply_Invalid_Patino()
+	public void Multiply_Invalid_Nelson()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Multiply("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Multiply("a", "1"));
@@ -75,7 +75,7 @@ public class Multiply
 	}
 
 	[TestMethod]
-	public void Multiply_Null_Patino()
+	public void Multiply_Null_Nelson()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null, "1"));
@@ -87,15 +87,15 @@ public class Multiply
 public class Divide
 {
 	[TestMethod]
-	public void Divide_Valid_Patino()
+	public void Divide_Valid_Nelson()
 	{
-		Assert.AreEqual(3, Program.Divide("1", "2"));
-		Assert.AreEqual(5, Program.Divide("3", "2"));
-		Assert.AreEqual(12, Program.Divide("5", "7"));
+		Assert.AreEqual(2, Program.Divide("2", "1"));
+		Assert.AreEqual(3, Program.Divide("6", "2"));
+		Assert.AreEqual(1, Program.Divide("5", "5"));
 	}
 
 	[TestMethod]
-	public void Divide_Invalid_Patino()
+	public void Divide_Invalid_Nelson()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Divide("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Divide("a", "1"));
@@ -103,7 +103,7 @@ public class Divide
 	}
 
 	[TestMethod]
-	public void Divide_Null_Patino()
+	public void Divide_Null_Nelson()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null, "1"));
@@ -115,15 +115,15 @@ public class Divide
 public class Power
 {
 	[TestMethod]
-	public void Power_Valid_Patino()
+	public void Power_Valid_Nelson()
 	{
-		Assert.AreEqual(3, Program.Power("1", "2"));
-		Assert.AreEqual(5, Program.Power("3", "2"));
-		Assert.AreEqual(12, Program.Power("5", "7"));
+		Assert.AreEqual(1, Program.Power("1", "2"));
+		Assert.AreEqual(9, Program.Power("3", "2"));
+		Assert.AreEqual(125, Program.Power("5", "3"));
 	}
 
 	[TestMethod]
-	public void Power_Invalid_Patino()
+	public void Power_Invalid_Nelson()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Power("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Power("a", "1"));
@@ -131,7 +131,7 @@ public class Power
 	}
 
 	[TestMethod]
-	public void Power_Null_Patino()
+	public void Power_Null_Nelson()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Power("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null, "1"));
